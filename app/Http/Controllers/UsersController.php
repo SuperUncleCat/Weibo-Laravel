b4145204b9e311090db7b7c03afea5e599558885
togotest2017 <togotest2017@yahoo.co.jp>
2017-12-20 14:01:39 +0900

    finish user signup

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,10 +3,34 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\User;
 
 class UsersController extends Controller
 {
     public function create(){
       return view('users.create');
+    }
+
+    public function show(User $user){
+      return view('users.show',compact('user'));
+    }
+
+    public function store(Request $request){
+      $this->validate($request,[
+        'name'=>'required|max:50',
+        'email'=>'required|email|unique:users|max:225',
+        'password'=>'required|confirmed',
+        'password_confirmation'=>'required'
+      ]);
+
+      $user=User::create([
+        'name'=>$request->name,
+        'email'=>$request->email,
+        'password'=>bcrypt($request->password),
+      ]);
+
+      session()->flash('success','Welcome,you will have a new trip')
+      return redirect()->route('users.show',[$user]);
     }
 }
--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,16 +1,40 @@
 @extends('layouts.default')
-
+@section('title','SignUp')
 @section('content')
-  <div class="jumbotron">
-    <h1>Hello Weibo</h1>
-    <p class="lead">
-      你现在所看到的是 <a href="https://fsdhub.com/books/laravel-essential-training-5.1">Weibo-Laravel</a> 的项目主页。
-    </p>
-    <p>
-      一切，将从这里开始。
-    </p>
-    <p>
-      <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">Sign Up</a>
-    </p>
+  <div class="col-md-offset-2 col-md-8">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h5>Sign Up</h5>
+      </div>
+      <div class="panel-body">
+        @include('shared._errors')
+        
+        <form method="POST" action="{{route('users.store')}}">
+          {{ csrf_field() }}
+
+          <div class="form-group">
+            <label for="name">Name:</label>
+            <input type="text" name="name" class="form-control" value="{{old('name')}}">
+          </div>
+
+          <div class="form-group">
+            <label for="email">Email:</label>
+            <input type="text" name="email" class="form-control" value="{{old('email')}}">
+          </div>
+
+          <div class="form-group">
+            <label for="password">Password:</label>
+            <input type="password" name="password" class="form-control" value="{{old('password')}}">
+          </div>
+
+          <div class="form-group">
+            <label for="password_confirmation">Confirmation:</label>
+            <input type="password" name="password_confirmation" class="form-control" value="{{old('password')}}">
+          </div>
+
+          <button type="submit" class="btn btn-primary">Sign Up</button>
+        </form>
+      </div>
+    </div>
   </div>
 @stop
