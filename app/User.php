a181fafddc52f9903f3c42ae9ae4ad30e178b0eb
SuperUncleCat <31983824+SuperUncleCat@users.noreply.github.com>
2025-07-25 13:14:17 +0900

    Fix follow/unfollow single ID handling

--- app/User.php
+++ app/User.php
@@ -70,16 +70,16 @@ class User extends Authenticatable
       return $this->belongsToMany(User::Class,'followers','follower_id','user_id');
     }
 
-    public function follow($user_ids){
+  public function follow($user_ids){
       if(!is_array($user_ids)){
-        $user_ids=compact('user_ids');
+        $user_ids=[ $user_ids ];
       }
       $this->followings()->sync($user_ids,false);
     }
 
     public function unfollow($user_ids){
       if(!is_array($user_ids)){
-        $user_ids=compact('user_ids');
+        $user_ids=[ $user_ids ];
       }
       $this->followings()->detach($user_ids);
     }
